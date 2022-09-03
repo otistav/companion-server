c7c8324479f800c09c9d7cfb2460e27307da2e86
Sam Bocharov <semen_bard@mail.ru>
2022-09-03 18:07:24 +0200

    init server

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
 import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res, next) => {
+  res.send('hello world');
+});
+
+app.listen(3000, () => {
+  console.log('app is running');
+});
